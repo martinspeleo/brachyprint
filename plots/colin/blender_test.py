11150ac686119858b0e6c4044dc7c08f4c696be8
Colin Wren <colin@gimpneek.com>
2013-05-26 00:22:21 +0100

    Dummy data into a NURBS line

--- plots/colin/blender_test.py
+++ plots/colin/blender_test.py
@@ -1,18 +1,33 @@
 import bpy
 import json
+from mathutils import Vector
 
+w = 1
 jsonD = []
-with open("/Users/colinwren/Documents/brachy/blender.json") as f:
+with open("/Users/colinwren/Documents/Development/resources/scripts/brachyprint/plots/colin/blender.json") as f:
 	jsonD = json.load(f)
 	
 verts = jsonD
-faces = []
-edges = []
-mesh = bpy.data.meshes.new('Test')
-mesh.from_pydata(verts,edges,faces)
+cList = []
+for d in jsonD:
+	cList.append(Vector((d[0], d[1], d[2])))
+	
+curvedata = bpy.data.curves.new(name='Test', type="CURVE")
+curvedata.dimensions = '3D'
 
-scene = bpy.context.scene
+obj = bpy.data.objects.new('Test',curvedata)
+obj.location = (0,0,0)
+bpy.context.scene.objects.link(obj)
 
-obj = bpy.data.objects.new('Test',mesh)
-scene.objects.link(obj)
-scene.update()
+polyline = curvedata.splines.new('NURBS')  
+polyline.points.add(len(cList)-1)  
+for num in range(len(cList)):  
+    x, y, z = cList[num]  
+    polyline.points[num].co = (x, y, z, w)
+polyline.points.add(len(cList))
+a,b,c = cList[0]
+polyline.points[len(cList)].co = (a,b,c,w)
+polyline.order_u = len(polyline.points)-1
+polyline.use_endpoint_u = True
+
+bpy.context.scene.update()
